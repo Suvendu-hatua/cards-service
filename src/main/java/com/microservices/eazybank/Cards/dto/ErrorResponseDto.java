2c3517ec855c65ccb96237bf8f216af4e6b812de
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-25 00:02:19 +0530

    Implemented Spring doc swagger documentation.

--- src/main/java/com/microservices/eazybank/Cards/dto/ErrorResponseDto.java
+++ src/main/java/com/microservices/eazybank/Cards/dto/ErrorResponseDto.java
@@ -1,9 +1,7 @@
 package com.microservices.eazybank.Cards.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
@@ -12,13 +10,30 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold ErrorResponse information"
+)
 public class ErrorResponseDto {
 
+    @Schema(
+            description ="API path invoked by client"
+    )
     private String apiPath;
 
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private HttpStatus httpStatus;
 
+    @Schema(
+            description = "Error message representing the error happened"
+    )
     private String message;
 
+    @Schema(
+            description = "Time representing when the error happened"
+    )
     private LocalDateTime timestamp;
 }
