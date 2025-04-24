315492d118c9b48083dc227ee8e895f3b046867c
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-25 00:02:04 +0530

    Implemented Spring doc swagger documentation.

--- src/main/java/com/microservices/eazybank/Cards/dto/CardsDto.java
+++ src/main/java/com/microservices/eazybank/Cards/dto/CardsDto.java
@@ -1,25 +1,58 @@
 package com.microservices.eazybank.Cards.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
 
+@Schema(
+        name = "Cards",
+        description = "Schema to hold Cards Information"
+)
 @Setter
 @Getter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class CardsDto {
 
+    @Schema(
+            description = "Card Number of the Customer",example = "100646930341"
+    )
+    @NotEmpty(message = "Card Number can not be a null or blank")
+    @Pattern(regexp = "(^$|[0-9]{12})",message = "CardNumber must be 12 digits")
     private String cardNumber;
 
+    @NotEmpty(message = "Mobile Number can not be a null or blank")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile Number must be 10 digits")
+    @Schema(
+            description = "Mobile Number of the Customer",example = "9876543210"
+    )
     private String mobileNumber;
 
+    @NotEmpty(message = "CardType can not be a null or blank")
+    @Schema(
+            description = "Type of the card", example = "Credit Card"
+    )
     private String cardType;
 
+    @PositiveOrZero(message = "Total available amount should be equal or greater than zero")
+    @Schema(
+            description = "Total available amount against a card", example = "90000"
+    )
     private int availableAmount;
 
+    @Positive(message = "Total card limit should be greater than zero")
+    @Schema(
+            description = "Total amount limit available against a card", example = "100000"
+    )
     private int totalLimit;
 
+    @PositiveOrZero(message = "Total amount used should be equal or greater than zero")
+    @Schema(
+            description = "Total amount used by a Customer", example = "1000"
+    )
     private int amountUsed;
 }
--- src/main/java/com/microservices/eazybank/Cards/dto/ResponseDto.java
+++ src/main/java/com/microservices/eazybank/Cards/dto/ResponseDto.java
@@ -1,5 +1,6 @@
 package com.microservices.eazybank.Cards.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +10,18 @@ import lombok.ToString;
 @Getter
 @ToString
 @AllArgsConstructor
+@Schema(
+        name = "Response",
+        description = "Schema to hold Successful response information"
+)
 public class ResponseDto {
+    @Schema(
+            description = "Status code in the response"
+    )
     private String httpStatus;
+
+    @Schema(
+            description = "Status message in the response"
+    )
     private String message;
 }
