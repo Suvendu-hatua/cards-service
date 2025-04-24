2f54cbe642c81a82878ca412b0c5ffca115919d7
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-25 00:04:06 +0530

    Implemented all the types of exception handler in GlobalExceptionHandler class

--- src/main/java/com/microservices/eazybank/Cards/exceptionHandling/ResourceNotFoundException.java
+++ src/main/java/com/microservices/eazybank/Cards/exceptionHandling/ResourceNotFoundException.java
@@ -6,10 +6,10 @@ public class ResourceNotFoundException extends RuntimeException {
      * The cause is not initialized, and may subsequently be initialized by a
      * call to {@link #initCause}.
      *
-     * @param message the detail message. The detail message is saved for
+     * @param resourceName the detail message. The detail message is saved for
      *                later retrieval by the {@link #getMessage()} method.
      */
-    public ResourceNotFoundException(String message) {
-        super(message);
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with the given input data %s : '%s'", resourceName, fieldName, fieldValue));
     }
 }
